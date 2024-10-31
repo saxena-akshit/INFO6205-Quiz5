343802a129d5ac99999892ad5306e34fb2b45443
saxena-akshit <saxena.aksh@northeastern.edu>
2024-10-31 16:14:57 -0400

    feat: implemented RB BST

--- src/main/java/RedBlackBST.java
+++ src/main/java/RedBlackBST.java
@@ -1,14 +1,16 @@
+
 import java.util.NoSuchElementException;
 
 public class RedBlackBST<Key extends Comparable<Key>, Value> {
 
-    private static final boolean RED   = true;
+    private static final boolean RED = true;
     private static final boolean BLACK = false;
 
     private Node root;     // root of the BST
 
     // BST helper node data type
     private class Node {
+
         private Key key;           // key
         private Value val;         // associated data
         private Node left, right;  // links to left and right subtrees
@@ -29,24 +31,30 @@ public class RedBlackBST<Key extends Comparable<Key>, Value> {
     public RedBlackBST() {
     }
 
-    /***************************************************************************
-     *  Node helper methods.
-     ***************************************************************************/
+    /**
+     * *************************************************************************
+     * Node helper methods.
+     * *************************************************************************
+     */
     // is node x red; false if x is null ?
     private boolean isRed(Node x) {
-        if (x == null) return false;
+        if (x == null) {
+            return false;
+        }
         return x.color == RED;
     }
 
     // number of node in subtree rooted at x; 0 if x is null
     private int size(Node x) {
-        if (x == null) return 0;
+        if (x == null) {
+            return 0;
+        }
         return x.size;
     }
 
-
     /**
      * Returns the number of key-value pairs in this symbol table.
+     *
      * @return the number of key-value pairs in this symbol table
      */
     public int size() {
@@ -55,26 +63,31 @@ public class RedBlackBST<Key extends Comparable<Key>, Value> {
 
     /**
      * Is this symbol table empty?
-     * @return {@code true} if this symbol table is empty and {@code false} otherwise
+     *
+     * @return {@code true} if this symbol table is empty and {@code false}
+     * otherwise
      */
     public boolean isEmpty() {
         return root == null;
     }
 
-
-    /***************************************************************************
-     *  Standard BST search.
-     ***************************************************************************/
-
+    /**
+     * *************************************************************************
+     * Standard BST search.
+     * *************************************************************************
+     */
     /**
      * Returns the value associated with the given key.
+     *
      * @param key the key
-     * @return the value associated with the given key if the key is in the symbol table
-     *     and {@code null} if the key is not in the symbol table
+     * @return the value associated with the given key if the key is in the
+     * symbol table and {@code null} if the key is not in the symbol table
      * @throws IllegalArgumentException if {@code key} is {@code null}
      */
     public Value get(Key key) {
-        if (key == null) throw new IllegalArgumentException("argument to get() is null");
+        if (key == null) {
+            throw new IllegalArgumentException("argument to get() is null");
+        }
         return get(root, key);
     }
 
@@ -82,40 +95,48 @@ public class RedBlackBST<Key extends Comparable<Key>, Value> {
     private Value get(Node x, Key key) {
         while (x != null) {
             int cmp = key.compareTo(x.key);
-            if      (cmp < 0) x = x.left;
-            else if (cmp > 0) x = x.right;
-            else              return x.val;
+            if (cmp < 0) {
+                x = x.left;
+            } else if (cmp > 0) {
+                x = x.right;
+            } else {
+                return x.val;
+            }
         }
         return null;
     }
 
     /**
      * Does this symbol table contain the given key?
+     *
      * @param key the key
      * @return {@code true} if this symbol table contains {@code key} and
-     *     {@code false} otherwise
+     * {@code false} otherwise
      * @throws IllegalArgumentException if {@code key} is {@code null}
      */
     public boolean contains(Key key) {
         return get(key) != null;
     }
 
-    /***************************************************************************
-     *  Red-black tree insertion.
-     ***************************************************************************/
-
     /**
-     * Inserts the specified key-value pair into the symbol table, overwriting the old
-     * value with the new value if the symbol table already contains the specified key.
-     * Deletes the specified key (and its associated value) from this symbol table
-     * if the specified value is {@code null}.
+     * *************************************************************************
+     * Red-black tree insertion.
+     * *************************************************************************
+     */
+    /**
+     * Inserts the specified key-value pair into the symbol table, overwriting
+     * the old value with the new value if the symbol table already contains the
+     * specified key. Deletes the specified key (and its associated value) from
+     * this symbol table if the specified value is {@code null}.
      *
      * @param key the key
      * @param val the value
      * @throws IllegalArgumentException if {@code key} is {@code null}
      */
     public void put(Key key, Value val) {
-        if (key == null) throw new IllegalArgumentException("first argument to put() is null");
+        if (key == null) {
+            throw new IllegalArgumentException("first argument to put() is null");
+        }
         if (val == null) {
             delete(key);
             return;
@@ -128,69 +149,106 @@ public class RedBlackBST<Key extends Comparable<Key>, Value> {
 
     // insert the key-value pair in the subtree rooted at h
     private Node put(Node h, Key key, Value val) {
-        //TO BE IMPLEMENTED
-        
+        if (h == null) {
+            return new Node(key, val, RED, 1);
+        }
+        int cmp = key.compareTo(h.key);
+        if (cmp < 0) {
+            h.left = put(h.left, key, val);
+        } else if (cmp > 0) {
+            h.right = put(h.right, key, val);
+        } else {
+            h.val = val;
+        }
+        if (isRed(h.right) && !isRed(h.left)) {
+            h = rotateLeft(h);
+        }
+        if (isRed(h.left) && isRed(h.left.left)) {
+            h = rotateRight(h);
+        }
+        if (isRed(h.left) && isRed(h.right)) {
+            flipColors(h);
+        }
+        h.size = size(h.left) + size(h.right) + 1;
+        return h;
     }
 
-    /***************************************************************************
-     *  Red-black tree deletion.
-     ***************************************************************************/
-
+    /**
+     * *************************************************************************
+     * Red-black tree deletion.
+     * *************************************************************************
+     */
     /**
      * Removes the smallest key and associated value from the symbol table.
+     *
      * @throws NoSuchElementException if the symbol table is empty
      */
     public void deleteMin() {
-        if (isEmpty()) throw new NoSuchElementException("BST underflow");
+        if (isEmpty()) {
+            throw new NoSuchElementException("BST underflow");
+        }
 
         // if both children of root are black, set root to red
-        if (!isRed(root.left) && !isRed(root.right))
+        if (!isRed(root.left) && !isRed(root.right)) {
             root.color = RED;
+        }
 
         root = deleteMin(root);
-        if (!isEmpty()) root.color = BLACK;
+        if (!isEmpty()) {
+            root.color = BLACK;
+        }
         // assert check();
     }
 
     // delete the key-value pair with the minimum key rooted at h
     private Node deleteMin(Node h) {
-        if (h.left == null)
+        if (h.left == null) {
             return null;
+        }
 
-        if (!isRed(h.left) && !isRed(h.left.left))
+        if (!isRed(h.left) && !isRed(h.left.left)) {
             h = moveRedLeft(h);
+        }
 
         h.left = deleteMin(h.left);
         return balance(h);
     }
 
-
     /**
      * Removes the largest key and associated value from the symbol table.
+     *
      * @throws NoSuchElementException if the symbol table is empty
      */
     public void deleteMax() {
-        if (isEmpty()) throw new NoSuchElementException("BST underflow");
+        if (isEmpty()) {
+            throw new NoSuchElementException("BST underflow");
+        }
 
         // if both children of root are black, set root to red
-        if (!isRed(root.left) && !isRed(root.right))
+        if (!isRed(root.left) && !isRed(root.right)) {
             root.color = RED;
+        }
 
         root = deleteMax(root);
-        if (!isEmpty()) root.color = BLACK;
+        if (!isEmpty()) {
+            root.color = BLACK;
+        }
         // assert check();
     }
 
     // delete the key-value pair with the maximum key rooted at h
     private Node deleteMax(Node h) {
-        if (isRed(h.left))
+        if (isRed(h.left)) {
             h = rotateRight(h);
+        }
 
-        if (h.right == null)
+        if (h.right == null) {
             return null;
+        }
 
-        if (!isRed(h.right) && !isRed(h.right.left))
+        if (!isRed(h.right) && !isRed(h.right.left)) {
             h = moveRedRight(h);
+        }
 
         h.right = deleteMax(h.right);
 
@@ -201,19 +259,26 @@ public class RedBlackBST<Key extends Comparable<Key>, Value> {
      * Removes the specified key and its associated value from this symbol table
      * (if the key is in this symbol table).
      *
-     * @param  key the key
+     * @param key the key
      * @throws IllegalArgumentException if {@code key} is {@code null}
      */
     public void delete(Key key) {
-        if (key == null) throw new IllegalArgumentException("argument to delete() is null");
-        if (!contains(key)) return;
+        if (key == null) {
+            throw new IllegalArgumentException("argument to delete() is null");
+        }
+        if (!contains(key)) {
+            return;
+        }
 
         // if both children of root are black, set root to red
-        if (!isRed(root.left) && !isRed(root.right))
+        if (!isRed(root.left) && !isRed(root.right)) {
             root.color = RED;
+        }
 
         root = delete(root, key);
-        if (!isEmpty()) root.color = BLACK;
+        if (!isEmpty()) {
+            root.color = BLACK;
+        }
         // assert check();
     }
 
@@ -221,18 +286,21 @@ public class RedBlackBST<Key extends Comparable<Key>, Value> {
     private Node delete(Node h, Key key) {
         // assert get(h, key) != null;
 
-        if (key.compareTo(h.key) < 0)  {
-            if (!isRed(h.left) && !isRed(h.left.left))
+        if (key.compareTo(h.key) < 0) {
+            if (!isRed(h.left) && !isRed(h.left.left)) {
                 h = moveRedLeft(h);
+            }
             h.left = delete(h.left, key);
-        }
-        else {
-            if (isRed(h.left))
+        } else {
+            if (isRed(h.left)) {
                 h = rotateRight(h);
-            if (key.compareTo(h.key) == 0 && (h.right == null))
+            }
+            if (key.compareTo(h.key) == 0 && (h.right == null)) {
                 return null;
-            if (!isRed(h.right) && !isRed(h.right.left))
+            }
+            if (!isRed(h.right) && !isRed(h.right.left)) {
                 h = moveRedRight(h);
+            }
             if (key.compareTo(h.key) == 0) {
                 Node x = min(h.right);
                 h.key = x.key;
@@ -240,26 +308,40 @@ public class RedBlackBST<Key extends Comparable<Key>, Value> {
                 // h.val = get(h.right, min(h.right).key);
                 // h.key = min(h.right).key;
                 h.right = deleteMin(h.right);
+            } else {
+                h.right = delete(h.right, key);
             }
-            else h.right = delete(h.right, key);
         }
         return balance(h);
     }
 
-    /***************************************************************************
-     *  Red-black tree helper functions.
-     ***************************************************************************/
-
+    /**
+     * *************************************************************************
+     * Red-black tree helper functions.
+     * *************************************************************************
+     */
     // make a left-leaning link lean to the right
     private Node rotateRight(Node h) {
-        //TO BE IMPLEMENTED
-       
+        Node x = h.left;
+        h.left = x.right;
+        x.right = h;
+        x.color = x.right.color;
+        x.right.color = RED;
+        x.size = h.size;
+        h.size = size(h.left) + size(h.right) + 1;
+        return x;
     }
 
     // make a right-leaning link lean to the left
     private Node rotateLeft(Node h) {
-        //TO BE IMPLEMENTED
-       
+        Node x = h.right;
+        h.right = x.left;
+        x.left = h;
+        x.color = x.left.color;
+        x.left.color = RED;
+        x.size = h.size;
+        h.size = size(h.left) + size(h.right) + 1;
+        return x;
     }
 
     // flip the colors of a node and its two children
@@ -305,49 +387,58 @@ public class RedBlackBST<Key extends Comparable<Key>, Value> {
     private Node balance(Node h) {
         // assert (h != null);
 
-        if (isRed(h.right) && !isRed(h.left))    h = rotateLeft(h);
-        if (isRed(h.left) && isRed(h.left.left)) h = rotateRight(h);
-        if (isRed(h.left) && isRed(h.right))     flipColors(h);
+        if (isRed(h.right) && !isRed(h.left)) {
+            h = rotateLeft(h);
+        }
+        if (isRed(h.left) && isRed(h.left.left)) {
+            h = rotateRight(h);
+        }
+        if (isRed(h.left) && isRed(h.right)) {
+            flipColors(h);
+        }
 
         h.size = size(h.left) + size(h.right) + 1;
         return h;
     }
 
-
-    /***************************************************************************
-     *  Utility functions.
-     ***************************************************************************/
-
+    /**
+     * *************************************************************************
+     * Utility functions.
+     * *************************************************************************
+     */
     /**
      * Returns the height of the BST (for debugging).
+     *
      * @return the height of the BST (a 1-node tree has height 0)
      */
     public int height() {
         return height(root);
     }
+
     private int height(Node x) {
-        if (x == null) return -1;
+        if (x == null) {
+            return -1;
+        }
         return 1 + Math.max(height(x.left), height(x.right));
     }
 
-
-
-
-
-
-
-
     private Node min(Node x) {
         // assert x != null;
-        if (x.left == null) return x;
-        else                return min(x.left);
+        if (x.left == null) {
+            return x;
+        } else {
+            return min(x.left);
+        }
     }
+
     // do all paths from root to leaf have same number of black edges?
     protected boolean isBalanced() {
         int black = 0;     // number of black links on path from root to min
         Node x = root;
         while (x != null) {
-            if (!isRed(x)) black++;
+            if (!isRed(x)) {
+                black++;
+            }
             x = x.left;
         }
         return isBalanced(root, black);
@@ -355,10 +446,13 @@ public class RedBlackBST<Key extends Comparable<Key>, Value> {
 
     // does every path from the root to a leaf have the given number of black links?
     private boolean isBalanced(Node x, int black) {
-        if (x == null) return black == 0;
-        if (!isRed(x)) black--;
+        if (x == null) {
+            return black == 0;
+        }
+        if (!isRed(x)) {
+            black--;
+        }
         return isBalanced(x.left, black) && isBalanced(x.right, black);
     }
-
 
 }
